8aa05be6b91a4bb8665a031c0ecfa47661cd814a
A_D <aunderscored@gmail.com>
2020-07-22 14:18:38 +0200

    Moved checking attrs and such to a util method
    This way multiple mocks can use this. Though Im considering replacing
    them with specced mocks anyway

--- tests/test_mock_monitor.py
+++ tests/test_mock_monitor.py
@@ -1,5 +1,4 @@
 import unittest
-import inspect
 
 import mock_monitor
 from monitor import EDLogs
@@ -19,30 +18,7 @@ class TestMockEDLog(unittest.TestCase):
         for key in to_test.state:
             self.assertIn(key, mock_monitor.DEFAULT_STATE)
 
-        for key in EDLogs.__dict__:
-            if key.startswith('_'):
-                # These are internal, anything accessing them ought to explode anyway
-                continue
-
-            assert key in dir(mock_monitor.MockEDLog), NOT_COMPAT + "Missing field {!r}".format(key)
-
-            attr = getattr(EDLogs, key)
-            mock_attr = getattr(mock_monitor.EDLogs, key)
-
-            if not callable(attr):
-                continue
-
-            assert callable(mock_attr), NOT_COMPAT + "Field {!r} exists but is not callable".format(key)
-
-            orig_params = inspect.signature(attr).parameters
-            orig_params = inspect.signature(mock_attr).parameters
-
-            assert len(orig_params) == len(orig_params), \
-                NOT_COMPAT + "Signature length for {!r} does not match".format(key)
-
-            for key in orig_params:
-                assert orig_params[key] == orig_params[key], \
-                    NOT_COMPAT + "signature for {!r} does not match".format(key)
+        test_util.test_mock_compat(EDLogs, mock_monitor.MockEDLog)
 
     def test_exploding_properties(self):
         to_test = mock_monitor.MockEDLog()
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,8 @@
 
+import inspect
+from typing import Any, ClassVar
+
+
 class TestingException(Exception):
     pass
 
@@ -26,3 +30,44 @@ def exploding_property(f):
         return actually_explode
 
     return property(fget=explode(name, 'setter'), fset=explode(name, 'get'), fdel=explode(name, 'del'), doc=f.__doc__)
+
+
+def get_name(thing) -> str:
+    if inspect.isclass(thing):
+        return thing.__name__
+    return thing.__class__.__name__
+
+
+def test_mock_compat(real, mocked, single_underscore=False, double_underscore=False):
+    """Compares a mocked object to the real version, ensuring that all fields are the same"""
+
+    NOT_COMPAT = f"mock {get_name(mocked)} is not compatible with {get_name(real)}: "
+
+    for key in real.__dict__:
+        if key.startswith('__') and not double_underscore:
+            continue
+        if key.startswith('_') and not single_underscore:
+            # These are internal, anything accessing them ought to explode anyway
+            continue
+
+        assert key in dir(mocked), NOT_COMPAT + f"Missing field {key!r}"
+
+        attr = getattr(real, key)
+        mock_attr = getattr(mocked, key)
+
+        if not callable(attr):
+            continue
+
+        if isinstance(mock_attr, property):
+            # Its callable, but its also a property, assume its been overridden in some way on the mock side
+            continue
+
+        assert callable(mock_attr), NOT_COMPAT + f"Field {key!r} exists but is not callable"
+
+        orig_params = inspect.signature(attr).parameters
+        orig_params = inspect.signature(mock_attr).parameters
+
+        assert len(orig_params) == len(orig_params), NOT_COMPAT + f"Signature length for {key!r} does not match"
+
+        for key in orig_params:
+            assert orig_params[key] == orig_params[key], NOT_COMPAT + f"signature for {key!r} does not match"
